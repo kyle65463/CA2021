600477244f1050f8570c40e0284d2ee1ea2842be
kyle65463 <kyle65463@gmail.com>
2022-01-17 17:20:05 +0800

    Complete basic dache_sram

--- lab2/codes/dcache_sram.v
+++ lab2/codes/dcache_sram.v
@@ -16,22 +16,24 @@ module dcache_sram
 input              clk_i;
 input              rst_i;
 input    [3:0]     addr_i;
-input    [24:0]    tag_i;
+input    [24:0]    tag_i; // 23-bit tag + 1 dirty bit + 1 valid bit
 input    [255:0]   data_i;
-input              enable_i;
-input              write_i;
+input              enable_i; // cpu_req
+input              write_i; // cache_write | (hit & cpu_MemWrite_i)
 
 output   [24:0]    tag_o;
 output   [255:0]   data_o;
 output             hit_o;
 
+reg   [24:0]    tag_o;
+reg   [255:0]   data_o;
+reg             hit_o;
 
 // Memory
 reg      [24:0]    tag [0:15][0:1];    
 reg      [255:0]   data[0:15][0:1];
 
 integer            i, j;
-
 
 // Write Data      
 // 1. Write hit
@@ -47,10 +49,26 @@ always@(posedge clk_i or posedge rst_i) begin
     end
     if (enable_i && write_i) begin
         // TODO: Handle your write of 2-way associative cache + LRU here
+        i = 1'b0;
+        tag[addr_i][i] = tag_i;
+        data[addr_i][i] = data_i;
     end
 end
 
 // Read Data      
-// TODO: tag_o=? data_o=? hit_o=?
+always@(tag_i or addr_i or enable_i) begin
+    hit_o = 1'b0;
+    data_o = 256'b0;
+    tag_o = 25'b0;
+    if (enable_i) begin
+        for (j=0;j<2;j=j+1) begin
+            if (tag[addr_i][j][22:0] == tag_i[22:0] && tag[addr_i][j][24]) begin
+                hit_o = 1'b1;
+                data_o = data[addr_i][j];
+                tag_o = tag_i;
+            end
+        end
+    end
+end
 
 endmodule
